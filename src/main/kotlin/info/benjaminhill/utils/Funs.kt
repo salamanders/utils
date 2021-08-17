d152f69621fd98f69b014ad026394769e13ee87b
Benjamin Hill <benjaminhill@gmail.com>
2021-08-17 09:26:55 -0700

    deep stack trace and version updates

--- src/main/kotlin/info/benjaminhill/utils/Funs.kt
+++ src/main/kotlin/info/benjaminhill/utils/Funs.kt
@@ -2,6 +2,7 @@ package info.benjaminhill.utils
 
 /**
  * Because things can fail.
+ * TODO: Random falloff
  */
 suspend fun <T> retryOrNull(maxRetries: Int = 1, f: suspend () -> T?): T? {
     var attemptNum = 0
